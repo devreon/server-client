b9eb80eb8c65f992ee720922727f999e998cb6c5
devre <sasha.lomachenkov@mail.ru>
2021-11-21 00:50:24 +0300

    integer values in input

--- visualisation.py
+++ visualisation.py
@@ -8,25 +8,36 @@ import dash_bootstrap_components as dbc
 import numpy as np
 import math
 
+X = []
+Y = []
+Z = []
+T = []
+
+X_input = []
+Y_input = []
+T_input = []
+Z_input = []
+
 
 # ---------- Initialize X, Y, Z ----------
+def f(x, y, t):
+    return (1 / 2 * (math.pow(x + y, 4) + math.pow(x - y, 4)))*t  # math.sin(x*x+y*y)/((x*x+y*y))
 
-def f(x, y):
-    return 1/2 * (math.pow(x+y, 4)+math.pow(x-y, 4))      # math.sin(x*x+y*y)/((x*x+y*y))
 
-X = np.arange(-10, 11, 1)
-Y = X
-Z = np.zeros((21,21))
 
-for i in range(21):
-    for j in range(21):
-        Z[i][j] = f(X[i], Y[j])
+def calculate(X,Y,Z,t):
+    for i in np.arange(len(X)):
+        for j in np.arange(len(Y)):
+            Z[i][j] = f(X[i], Y[j],t)
+
+
+
+
 
 # ---------- Generate graphs ----------
+layout = {'title': {'text': '3D'}}
 
-layout = {'title': {'text':'3D'}}
-
-fig = go.Figure(data=[go.Surface(x = X, y = Y, z = Z)], layout=layout)
+fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)], layout=layout)
 
 # ---------- Display ----------
 
@@ -45,7 +56,8 @@ badge = dbc.Button(
 app.layout = html.Div([
     # graph
     html.Div(  # size of plot in style
-        children=[dcc.Graph(id='my-graph', figure=fig, style={'width': '50vh', 'height': '50vh', 'display':'inline-block'})]
+        #children=[dcc.Graph(id='my-graph', figure=fig, style={'width': '50vh', 'height': '50vh', 'display':'inline-block'})]
+        dcc.Graph(id='my-graph', figure=fig, style={'width': '50vh', 'height': '50vh', 'display':'inline-block'})
     ),
     # slider
 
@@ -56,7 +68,7 @@ app.layout = html.Div([
         step=0,
         value=0,
 
-        marks={i: '{}'.format(i) for i in range(1 + 10)},
+        marks={},
     ),
 
     html.Div(id='slider-output-container'),
@@ -101,10 +113,29 @@ app.layout = html.Div([
      ],
 
 )
+
+
 def update_output(n_clicks, x_start, x_change, x_end, y_start, y_change, y_end, t_start, t_change, t_end):
     # так можно доставать вытащить значения для callback элемента.
     # X=value
     # print(X)
+    if not n_clicks:
+        return( x_start, x_change, x_end, y_start, y_change, y_end, t_start, t_change, t_end)
+
+    print(x_start)
+    print(x_change)
+    print(x_end)
+    for i in np.arange(x_start, x_end, x_change):
+        X_input.append(i)
+    for i in np.arange(y_start, y_end, y_change):
+        Y_input.append(i)
+    for i in np.arange(t_start, t_end, t_change):
+        T_input.append(i)
+
+
+
+
+
     return 'The input value was {}_____{}_____{}_____{}_____{}_____{}_____{}_____{}_____{}_____  '.format(
          x_start, x_change, x_end,y_start, y_change, y_end, t_start, t_change, t_end, n_clicks
 
@@ -114,14 +145,12 @@ def update_output(n_clicks, x_start, x_change, x_end, y_start, y_change, y_end, 
 @app.callback(
     dash.dependencies.Output("my-slider", "min"),
     dash.dependencies.Output("my-slider", "max"),
-
     dash.dependencies.Output("my-slider", "value"),
     dash.dependencies.Output("my-slider", "step"),
     dash.dependencies.Output("my-slider", "marks"),
     dash.dependencies.Input("submit", "n_clicks"),
     dash.dependencies.State("my-slider", "min"),
     dash.dependencies.State("my-slider", "max"),
-
     dash.dependencies.State("my-slider", "value"),
     dash.dependencies.State("my-slider", "step"),
     dash.dependencies.State("my-slider", "marks"),
@@ -134,14 +163,14 @@ def update_slider(nClicks, sliderMin, sliderMax, sliderValue, sliderStep, slider
     if not nClicks:
         return (sliderMin, sliderMax, sliderValue,sliderStep, sliderMarks)
 
+    if isinstance(t_change, int):
+        return (t_start, t_end, 0, t_change, {i: '{}'.format(i) for i in range(t_start, t_end + t_change, t_change)})
+    else:
+        return (t_start, t_end, 0, t_change, {i: '{}'.format(i) for i in np.arange(t_start, t_end + t_change, t_change)})
 
-    return (t_start, t_end, 0, t_change, {i: '{}'.format(i) for i in range(t_start, t_end + t_change,t_change)})
 
 
-
-
-
-# slider callback for t .
+# slider callback for t .  graphic
 @app.callback(
     dash.dependencies.Output('slider-output-container', 'children'),
     dash.dependencies.Input('my-slider', 'value')
@@ -151,6 +180,37 @@ def update_output(value):
     # printedvalue=value
     # print(printedvalue)
     return 'You have selected t="{}"'.format(value)
+
+
+@app.callback(dash.dependencies.Output('my-graph', 'figure'),
+              [dash.dependencies.Input('my-slider', 'value')]
+              )
+def update_graph(value):
+    tvalue = value
+
+    Z_input = np.zeros((len(X_input), len(X_input)))
+
+    X = X_input
+    Y = Y_input
+    Z = Z_input
+
+    calculate(X, Y, Z, tvalue)
+    # X=  X_input
+    # Y = Y_input
+    # Z = Z_input
+
+
+    return go.Figure(data=[go.Surface(x=X, y=Y, z=Z)], layout=layout)
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
